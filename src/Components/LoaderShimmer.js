691f487103027ad09ba307282d2335945d8dd2ad
Himanshu <himanshu.singh_iot18@gla.ac.in>
2023-10-15 17:44:14 +0530

    intial code for news app

---
+++ src/Components/LoaderShimmer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ShimmerSimpleGallery } from "react-shimmer-effects";
+
+const LoaderShimmer = () => {
+    return (
+        <>
+          <ShimmerSimpleGallery card imageHeight={300} caption/>   
+        </>
+      );
+}
+
+export default LoaderShimmer
